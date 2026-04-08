66c79ae28fff59304e3d0091144ec339bec00e9d
alicecalabrese <alice.calabrese@mail.polimi.it>
2026-04-08 17:41:05 +0200

    use #if to conditionally show html

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,10 @@
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 <ClicksCounter bind:initialCount={count} />
-<p>count from parent is {count}</p>
+{#if count > 10 && count < 20}
+    <p>count from parent is {count}</p>
+{/if}
+
 
 <CanvasSquare/>
 
